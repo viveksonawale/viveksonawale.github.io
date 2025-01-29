f8feffb214bf498c42d7c2f8cf0d02c10b06e3bd
viveksonawale <sonawalevivek777@gmail.com>
2025-01-29 16:40:14 +0530

    added media Query

--- script.js
+++ script.js
@@ -23,3 +23,33 @@ function sendMail() {
     })
     .catch((err) => console.log("Error sending email: ", err));
 }
+
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+let sections = document.querySelector("section");
+let navLinks = document.querySelector("header nav");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+
+        let activeLink = document.querySelector(`header nav a[href*='${id}']`);
+        if (activeLink) {
+          activeLink.classList.add("active");
+        }
+      });
+    }
+  });
+};
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
